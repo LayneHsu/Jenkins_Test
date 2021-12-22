b028d7524b27fc02cb3bb67b7322c2e62d971768
LayneHsu <32662292+LayneHsu@users.noreply.github.com>
2021-12-23 00:19:36 +0800

    Update Houdini_Test.py

--- Houdini_Test.py
+++ Houdini_Test.py
@@ -2,9 +2,13 @@ import hou
 
 # Get scene root node
 sceneRoot = hou.node('/obj/')
+
 # Create empty geometry node in scene root
 geometry = sceneRoot.createNode('geo', run_init_scripts=False)
+
 # Set geometry node name
 geometry.setName('MY_GEO')
 
 geometry.createNode('box',"my_box")
+
+hou.hipFile.save("C:\Users\LayneHsu\Desktop\My_box", save_to_recent_files=True)
